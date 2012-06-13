5e57f4761ecd7ad785fdd7fea45f0f6ec29365bd
bouzuya <bouzuya@gmail.com>
2012-06-14 00:56:56 +0900

    update thymeleaf version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tleaf "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.thymeleaf/thymeleaf "1.0.0-beta1"]]
+                 [org.thymeleaf/thymeleaf "2.0.8"]]
   :main tleaf.core)
