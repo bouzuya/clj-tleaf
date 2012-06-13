995a82376ba6ba30e37507bb974b437519111feb
bouzuya <bouzuya@gmail.com>
2012-06-14 07:02:35 +0900

    add ${} example.

--- src/tleaf/core.clj
+++ src/tleaf/core.clj
@@ -1,17 +1,30 @@
 (ns tleaf.core
   (:gen-class)
-  (:import (org.thymeleaf TemplateEngine)
+  (:import (java.util Date)
+           (org.thymeleaf TemplateEngine)
            (org.thymeleaf.context Context)
            (org.thymeleaf.resourceresolver FileResourceResolver)
            (org.thymeleaf.templateresolver TemplateResolver)))
 
-(defn -main
-  [& args]
+(defn create-engine
+  []
   (let [tr (TemplateResolver.)]
     (.setResourceResolver tr (FileResourceResolver.))
+    (.setTemplateMode tr "XHTML")
+    (.setPrefix tr "src/tleaf/")
+    (.setSuffix tr ".html")
     (let [engine (TemplateEngine.)]
       (.setTemplateResolver engine tr)
-      (let [context (Context.)]
-        (print (.process engine "src/tleaf/index.html" context))))))
+      engine)))
+
+(defn create-context
+  [m]
+  (reduce (fn [c [k v]] (.setVariable c k v) c) (Context.) m))
+
+(defn -main
+  [& args]
+  (let [engine (create-engine)
+        context (create-context {"variable" "foobarbaz" "datetime" (.toString (Date.))})]
+    (print (.process engine "index" context))))
 
 
