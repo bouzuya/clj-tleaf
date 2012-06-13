05fb292001e292815d38def4fca0afd0c8264f53
bouzuya <bouzuya@gmail.com>
2012-06-14 07:01:41 +0900

    add deps org.slf4j/slf4j-nop

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject tleaf "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.slf4j/slf4j-nop "1.6.4"]
                  [org.thymeleaf/thymeleaf "2.0.8"]]
   :main tleaf.core)
